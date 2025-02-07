77c6f5fd8f3b3de693eb009466fc46236b6076ba
jcsalerno <contato.jcsalerno@gmail.com>
2025-02-07 20:31:29 -0300

    feat: Mapeando DTOs e Documents e Repositories do microsserviço

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
@@ -3,6 +3,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,8 +13,10 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Document (collection = "event")
 public class Event {
 
+    @Id
     private String id;
     private String transactionId;
     private String orderId;
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -3,6 +3,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,7 +13,10 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "order")
 public class Order {
+
+    @Id
 
     private String id;
     private List<OrderProducts> products;
