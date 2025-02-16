1a454949fb7126688a16ce0fbf0cda135672723a
jcsalerno <contato.jcsalerno@gmail.com>
2025-02-16 17:10:20 -0300

    feat: Criando classe responsável pela orquestração (OrchestrationService

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Event.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Event.java
@@ -5,9 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Data
 @Builder
@@ -24,4 +25,10 @@ public class Event {
     private List<History> eventHistory;
     private LocalDateTime createdAt;
 
+    public void addToHistory(History history) {
+        if (isEmpty(eventHistory)) {
+            eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 }
